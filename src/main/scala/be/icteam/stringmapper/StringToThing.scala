82dbcac36993d0b16f7eecf84a80e6d32cd5890b
Tim Van Wassenhove <tim@timvw.be>
2020-11-02 08:00:18 +0100

    added some test for anyvalues

--- src/main/scala/be/icteam/stringmapper/StringToThing.scala
+++ src/main/scala/be/icteam/stringmapper/StringToThing.scala
@@ -11,7 +11,7 @@ trait StringToThing[T] {
 object StringToThing {
 
     def apply[T](fn: String => T): StringToThing[T] = new StringToThing[T] {
-        def map(value: String) = {
+        def map(value: String): MapResult[T] = {
             Try {
                 fn(value)
             } match {
@@ -21,12 +21,19 @@ object StringToThing {
         }
     }
 
-    implicit val stringToString: StringToThing[String] = StringToThing[String](identity)
-    implicit val stringToInt: StringToThing[Int] = StringToThing[Int](_.toInt)
-    implicit val stringToDouble: StringToThing[Double] = StringToThing[Double](_.toDouble)
+    def to[T](fn: String => T): StringToThing[T] = StringToThing[T](fn)
+
+    implicit val stringToDouble: StringToThing[Double] = to[Double](_.toDouble)
+    implicit val stringToFloat: StringToThing[Float] = to[Float](_.toFloat)
+    implicit val stringToLong: StringToThing[Long] = to[Long](_.toLong)
+    implicit val stringToInt: StringToThing[Int] = to[Int](_.toInt)
+    implicit val stringToShort: StringToThing[Short] = to[Short](_.toShort)
+    implicit val stringToByte: StringToThing[Byte] = to[Byte](_.toByte)
+    implicit val stringToBoolean: StringToThing[Boolean] = to[Boolean](_.toBoolean)
+    implicit val stringToString: StringToThing[String] = to[String](identity)
 
     implicit def stringToOption[T: StringToThing]: StringToThing[Option[T]] = new StringToThing[Option[T]] {
-        def map(value: String) = {
+        def map(value: String): MapResult[Option[T]] = {
             if (value.isEmpty) {
                 MapResult.success(None)
             }
@@ -38,7 +45,7 @@ object StringToThing {
     }
 
     implicit def stringTofieldType[K, H](implicit hParser: StringToThing[H]): StringToThing[FieldType[K, H]] = new StringToThing[FieldType[K, H]] {
-        def map(line: String) = {
+        def map(line: String): MapResult[FieldType[K, H]] = {
             hParser.map(line) match {
                 case Left(msgs) => MapResult.failure(msgs: _*)
                 case Right(h) => MapResult.success(field[K].apply(h))
--- src/test/scala/be/icteam/stringmapper/StringToThingTests.scala
+++ src/test/scala/be/icteam/stringmapper/StringToThingTests.scala
@@ -4,9 +4,21 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class StringToThingTests extends AnyFunSuite {
 
-  test("should map to string as expected") {
-    val sut = implicitly[StringToThing[String]]
-    assert(sut.map("X") == Right("X"))
+  test("should map double as expected") {
+    val sut = implicitly[StringToThing[Double]]
+    assert(sut.map("3.0") == Right(3.0))
+    assert(sut.map("X") == Left(List("""java.lang.NumberFormatException: For input string: "X"""")))
+  }
+
+  test("should map float as expected") {
+    val sut = implicitly[StringToThing[Float]]
+    assert(sut.map("3.0") == Right(3.0f))
+    assert(sut.map("X") == Left(List("""java.lang.NumberFormatException: For input string: "X"""")))
+  }
+
+  test("should map long as expected") {
+    val sut = implicitly[StringToThing[Long]]
+    assert(sut.map("4") == Right(4L))
   }
 
   test("should map int as expected") {
@@ -15,10 +27,28 @@ class StringToThingTests extends AnyFunSuite {
     assert(sut.map("X") == Left(List("""java.lang.NumberFormatException: For input string: "X"""")))
   }
 
-  test("should map double as expected") {
-    val sut = implicitly[StringToThing[Double]]
-    assert(sut.map("3.0") == Right(3.0))
+  test("should map short as expected") {
+    val sut = implicitly[StringToThing[Short]]
+    assert(sut.map("3") == Right(3))
     assert(sut.map("X") == Left(List("""java.lang.NumberFormatException: For input string: "X"""")))
+  }
+
+  test("should map byte as expected") {
+    val sut = implicitly[StringToThing[Byte]]
+    assert(sut.map("1") == Right(1))
+    assert(sut.map("256") == Left(List("""java.lang.NumberFormatException: Value out of range. Value:"256" Radix:10""")))
+  }
+
+  test("should map boolean as expected") {
+    val sut = implicitly[StringToThing[Boolean]]
+    assert(sut.map("true") == Right(true))
+    assert(sut.map("false") == Right(false))
+    assert(sut.map("256") == Left(List("""java.lang.IllegalArgumentException: For input string: "256"""")))
+  }
+
+  test("should map to string as expected") {
+    val sut = implicitly[StringToThing[String]]
+    assert(sut.map("X") == Right("X"))
   }
 
   test("should map option as expected") {
